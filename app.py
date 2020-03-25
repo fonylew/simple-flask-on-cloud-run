a2af10c1a509a103529c884dec22fd8ef784f1c7
Kamolphan Lewprasert <2770263+fonylew@users.noreply.github.com>
2020-03-25 15:49:50 +0700

    removed unused function

--- app.py
+++ app.py
@@ -19,31 +19,6 @@ def currency_all():
     return df.to_json(date_format='iso')
 
 
-# @app.route('/currency_gbp')
-# def currency():
-#     query_parameters = request.args
-#
-#     date = query_parameters.get('date')
-#     month = query_parameters.get('month')
-#     year = query_parameters.get('year')
-#
-#
-#     if year is None or (month is None and year is None):
-#         return bad_request(400)
-#
-#     if not 2018 <= int(year) <= 2020 and not 1 <= int(month) <= 12:
-#         return bad_request(400)
-#
-#
-#
-#     if not 2018 <= int(year) <= 2020 and not 1 <= int(month) <= 12 and not 1 <= int(date) <= 31:
-#         return bad_request(400)
-#
-#     if year and month and date:
-#         return df[int(datetime.datetime(int(year), int(month), int(date), 0, 0).timestamp())].to_json(date_format='iso')
-#     else:
-#         return df.to_json(date_format='iso')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
